95120a26257e2f97b869c66e66693a6c5d775be6
skusmakar2 <71484991+skusmakar2@users.noreply.github.com>
2020-11-23 14:17:17 +1100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='The quit attempt fall back probability is $ {}'.format(output))
+    return render_template('index.html', prediction_text='The quit attempt fall back probability is {}'.format(output))
 
 
 if __name__ == "__main__":
